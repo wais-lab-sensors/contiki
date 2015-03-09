1d06292488428072b902a6777b70eccfba4fd4fe
Philip Basford <pjb@ecs.soton.ac.uk>
2015-03-09 17:39:59 +0000

    Will now print out converted temp - needs checking for accuracy

--- wais/sht25-test/sht25-test.c
+++ wais/sht25-test/sht25-test.c
@@ -5,16 +5,33 @@
 PROCESS(test_sht25_process, "SHT25 test");
 AUTOSTART_PROCESSES(&test_sht25_process);
 
+float
+floor(float x)
+{
+    if(x>=0.0f){
+        return (float) ((int)x);
+    } else{
+        return (float) ((int)x-1);
+    }
+}
+
 PROCESS_THREAD(test_sht25_process, ev, data)
 {
     static struct etimer et;
+    static uint16_t raw_temp;
+    static uint16_t raw_hum;
+    static float temp;
+    static float hum;
     PROCESS_BEGIN();
     sht25_init();
     printf("Starting loop\n");
     while(1){
         etimer_set(&et, CLOCK_SECOND);
         PROCESS_YIELD();
-        printf("Raw temp =  %u \n", (unsigned) (sht25_temp()));
+        raw_temp = sht25_temp();
+        printf("Raw temp =  %d \n",  (raw_temp));
+        temp = SHT2x_CalcTemperatureC(raw_temp);
+        printf("Temp = %ld.%03d\n", (long)temp, (unsigned)((temp-floor(temp)) * 1000));
         //rh = sht25_humidity();
         //printf("Rel. humidity: %u%%\n",
         //    (unsigned) (SHT2x_CalcRH(rh)));
